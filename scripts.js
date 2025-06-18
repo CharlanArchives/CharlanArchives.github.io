ec7df690182d9ebea36369df9df9dee39ec8c0d7
CharlanArchives <charlanarchive@gmail.com>
2025-06-18 15:35:10 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ async function searchYearbook(query, year) {
 
 const yearbookYears = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
 
-function searchAllYearbooks(query) {
+async function searchAllYearbooks(query) {
     var results = [];
 
     for (const year of yearbookYears) {
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var query = params.get("query");
 
     if (query) {
-        var yearbooks = searchAllYearbooks(query);
+        var yearbooks = await searchAllYearbooks(query);
         var resultsGrid = document.getElementsByClassName("results-grid")[0];
 
         console.log(yearbooks);
