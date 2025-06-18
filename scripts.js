8dc9573d0ad709f6d7f43bb368bcddf7b44df93f
CharlanArchives <charlanarchive@gmail.com>
2025-06-18 16:28:21 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ async function fetchTextFile(url) {
 async function searchYearbook(query, year) {
     // Example usage:
     var text = await fetchTextFile('https://charlanarchives.github.io/archive/yearbooks/searchtext/' + year + '.pdf.txt');
-    return text.includes(query);
+    return text.toLowerCase().includes(query.toLowerCase());
 }
 
 const yearbookYears = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
